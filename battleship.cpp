7acec6b76536a04f085468f70d527b2bb58a70b2
alexdreyesz <alexdreyesz@gmail.com>
2025-03-02 17:14:16 -0500

    Added changes

--- battleship.cpp
+++ battleship.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cstring>
+#include <thread> // Threading Library To Handle Threading 
+#include <mutex> // Mutex Library To Apply Locks And Mutes 
+#include <chrono> // //Chrono Library Calculate Execution Time And Work With Time
 using namespace std;
 
 // USER Player1 And AI Player2 
@@ -14,29 +17,59 @@ const char SHIP = 'S';
 const char HIT = 'X';
 const char MISS = 'O';
 
-// Battleship Pieces
-int carrier1[5][2]; // USER Carrier Ship
-int battleship1[4][2]; // USER Battleship Ship
-int cruiser1[3][2]; // USER Cruiser Ship
-int submarine1[2][2]; // USER Submarine Ship
-int destroyer1[1][2]; // USER Destroyer Ship
-int carrier2[5][2]; // AI Carrier Ship
-int battleship2[4][2]; // AI Battleship Ship
-int cruiser2[3][2]; // AI Cruiser Ship
-int submarine2[2][2]; // AI Submarine Ship
-int destroyer2[1][2]; // AI Destroyer Ship
+// Battleship Pieces 
+// First Index Ships X Coordinate 
+// Second Index Ships Y Coordinate
+// Third Index Current Player 
 
-void printBoard(char board[10][10], int playerNumber); // Print Battleship Board
+// Player Battle Ships
+int carrier1[5][2][2]; // USER Carrier Ship - 5 Holes
+int battleship1[4][2][2]; // USER Battleship Ship - 4 Holes
+int cruiser1[3][2][2]; // USER Cruiser Ship - 3 Holes
+int submarine1[3][2][2]; // USER Submarine Ship - 3 Holes
+int destroyer1[1][2][2]; // USER Destroyer Ship - 2 Holes
+
+// AI Battle Ships
+int carrier2[5][2][2]; // AI Carrier Ship -4 Holes
+int battleship2[4][2][2]; // AI Battleship Ship - 4 Holes
+int cruiser2[3][2][2]; // AI Cruiser Ship - 3 Holes
+int submarine2[3][2][2]; // AI Submarine Ship - 3 Holes
+int destroyer2[1][2][2]; // AI Destroyer Ship - 2 Holes
+
 void initializeBoard(char playerBoard[10][10]); // Initialize Player # Battleship Board
+void printBoard(char board[10][10], int playerNumber); // Print Battleship Board
+void monteCarloTreeSearch(char playerBoard[10][10]); // Monte Carlo Tree Search 
 
 int main() {
-    initializeBoard(boardPlayer1); // Initialize All Empty Spaces From Player 1 Board To Water Spaces '~'
-    initializeBoard(boardPlayer2); // Initialize All Empty Spaces From Player 2 Board To Water Spaces '~'
+    //initializeBoard(boardPlayer1); // Initialize All Empty Spaces From Player 1 Board To Water Spaces '~'
+    //initializeBoard(boardPlayer2); // Initialize All Empty Spaces From Player 2 Board To Water Spaces '~'
+
+    char boardPlayer1[10][10] = {
+        {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
+        {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
+        {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
+        {'~', '~', 'X', 'X', 'X', 'X', 'X', '~', '~', '~'},
+        {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
+        {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
+        {'~', '~', '~', '~', '~', '~', '~', '~', '~', '~'},
+        {'~', 'X', '~', '~', '~', '~', '~', '~', '~', '~'},
+        {'~', 'X', '~', '~', '~', '~', '~', '~', '~', 'X'},
+        {'~', '~', '~', '~', '~', '~', '~', '~', '~', 'X'}
+    };
 
     printBoard(boardPlayer1, player1); // Print Player 1 Board 
-    printBoard(boardPlayer2, player2); // Print Player 1 Board 
+    //printBoard(boardPlayer2, player2); // Print Player 1 Board 
 
     return 0;
+}
+
+// Initialize Player # Battleship Board
+void initializeBoard(char playerBoard[10][10]) {
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            playerBoard[i][j] = WATER;
+        }
+    }
 } 
 
 // Print Battleship Board
@@ -71,11 +104,7 @@ void printBoard(char board[10][10], int playerNumber) {
     cout << endl << endl;
 }
 
-// Initialize Player # Battleship Board
-void initializeBoard(char playerBoard[10][10]) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            playerBoard[i][j] = WATER;
-        }
-    }
-} 
+// Monte Carlo Tree Search
+void monteCarloTreeSearch(char playerBoard[10][10]) {
+
+}  
